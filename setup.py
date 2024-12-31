ddff494a4e99341c323579ad1b9a6c6720294a69
rentedninja <allanrodrigues0506@gmail.com>
2024-12-31 15:29:45 +0100

    logging and exception

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT ='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup (
+
+name='mlproject',
+version='0.0.1',
+author='Allan',
+author_email='allanrodrigues3568@gmail.com'
+packages =find_packages(),
+#install_requires = ['pandas','numpy','seaborn'],
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+
+)
