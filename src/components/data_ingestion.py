ff3913d364f3e35c7addd550933f642db1951b0f
rentedninja <allanrodrigues0506@gmail.com>
2025-01-08 16:45:40 +0100

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,11 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -81,6 +86,12 @@ if __name__ == "__main__":
         train_data, test_data = obj.initiate_data_ingestion()
         data_transformation = DataTransformation()
         train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+        modeltrainer=ModelTrainer()
+        modeltrainer.initiate_model_trainer(train_arr,test_arr)
+
+        modeltrainer=ModelTrainer()
+        print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
         logging.info(f"Train data path: {train_data}")
         logging.info(f"Test data path: {test_data}")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import os
 import sys
-import logging
+
 import pickle
+import dill
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -14,3 +17,34 @@ def save_object(file_path, obj):
         print(f"Object saved successfully at: {file_path}")
     except Exception as e:
         print(f"Error: {e}")
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
+
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
+
+            #model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
